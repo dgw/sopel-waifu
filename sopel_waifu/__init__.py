6db4ea2870551ccb6b51747a1e4bac266a316fe8
dgw <dgw@technobabbl.es>
2020-11-20 01:33:19 -0600

    Implement a basic waifu-suggestion system
    - Users `.addwaifu` with a description of who they want
    - The bot stores the list in memory
    - At shutdown, the list is persisted to a plugin DB value
    - At startup, the list is restored from plugin DB value
    - Admins can dump the list to disk & clear it via admin-only commands
    - The file is appended (not overwritten if it exists) in Sopel's homedir
    
    It would be fairly complicated to put together a usable syntax for
    interactively building an extra JSON file (dict) via IRC, so this is
    good enough. Admins can go through the suggestions periodically (or when
    users complain that it's been "forever") and build JSON themselves.

--- sopel_waifu/__init__.py
+++ sopel_waifu/__init__.py
@@ -23,6 +23,8 @@ class WaifuSection(config.types.StaticSection):
     """How the file specified by json_path should affect the default list."""
     unique_waifus = config.types.ValidatedAttribute('unique_waifus', bool, default=True)
     """Whether to deduplicate the waifu list during startup."""
+    accept_suggestions = config.types.ValidatedAttribute('accept_suggestions', bool, default=False)
+    """Whether to accept waifu suggestions via the `.addwaifu` command."""
 
 
 def setup(bot):
@@ -68,8 +70,19 @@ def setup(bot):
         if 'Fate/Grand Order' in waifu
     ]
 
+    if bot.config.waifu.accept_suggestions:
+        LOGGER.debug("Waifu suggestions are enabled; loading suggestion cache")
+        bot.memory['waifu-suggestions'] = bot.db.get_plugin_value(
+            'waifu', 'suggestions', [])
+
 
 def shutdown(bot):
+    if 'waifu-suggestions' in bot.memory:
+        LOGGER.debug("Persisting waifu suggestion cache to database...")
+        bot.db.set_plugin_value('waifu', 'suggestions', bot.memory['waifu-suggestions'])
+        del bot.memory['waifu-suggestions']
+        LOGGER.debug("...done!")
+
     for key in ['waifu-list', 'waifu-list-fgo']:
         try:
             del bot.memory[key]
@@ -103,3 +116,67 @@ def waifu(bot, trigger):
         msg = '{target}, your waifu is {waifu}'
 
     bot.say(msg.format(target=target, waifu=choice))
+
+
+@module.commands('addwaifu')
+@module.example('.addwaifu Holo from Spice & Wolf')
+@module.require_chanmsg('No hiding your perverted waifu preferences in PM!')
+def add_waifu(bot, trigger):
+    """Suggest a waifu for the bot admin to add in custom list."""
+    if not bot.config.waifu.accept_suggestions:
+        bot.reply("Waifu suggestions aren't enabled.")
+        return
+
+    new_waifu = trigger.group(2)
+    if not new_waifu:
+        bot.reply("Who did you want to suggest?")
+        return module.NOLIMIT
+
+    try:
+        bot.memory['waifu-suggestions'].append(new_waifu)
+    except Exception:
+        bot.reply("I'm terribly sorry, but something has gone very wrong. "
+                  "Please notify my owner, {}".format(bot.config.core.owner))
+        return
+    else:
+        bot.say("Recorded {}'s suggestion for a new waifu: {}".format(trigger.nick, new_waifu))
+
+
+@module.commands('dumpwaifus')
+@module.require_admin
+def dump_waifus(bot, trigger):
+    """Dump the list of suggested waifus to a file in Sopel's homedir."""
+    if 'waifu-suggestions' not in bot.memory or not bot.memory['waifu-suggestions']:
+        bot.reply("No waifu suggestions to dump.")
+        return
+
+    filename = os.path.join(bot.config.core.homedir, 'suggested-waifus.txt')
+
+    try:
+        with open(filename, 'a') as file:
+            for waifu in bot.memory['waifu-suggestions']:
+                file.write(waifu + '\n')
+    except Exception:
+        bot.reply("Sorry, something went wrong.")
+        return
+    else:
+        bot.reply("Dumped waifu suggestions to file. Location sent separately in PM.")
+        bot.say("Waifu suggestion file: {}".format(filename), trigger.nick)
+
+
+@module.commands('clearwaifus')
+@module.require_admin('Only a bot admin can clear my waifu suggestion list.')
+def clear_suggestions(bot, trigger):
+    """Clear the waifu suggestion cache."""
+    if 'waifu-suggestions' in bot.memory:
+        if bot.memory['waifu-suggestions']:
+            LOGGER.info('Cached waifu suggestions:')
+            for waifu in bot.memory['waifu-suggestions']:
+                LOGGER.info('    %s', waifu)
+        LOGGER.debug('Clearing waifu suggestion cache in memory')
+        bot.memory['waifu-suggestions'] = []
+
+    LOGGER.debug('Deleting saved waifu suggestions from bot DB')
+    bot.db.delete_plugin_value('waifu', 'suggestions')
+
+    bot.reply("Cleared waifu suggestions.")
